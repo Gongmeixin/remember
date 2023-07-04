3159cc13cc835399f89e0001db2ef3f727b3eb7e
哄哄 <1623511442@qq.com>
2023-07-04 17:05:33 +0800

    上传初始项目

---
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,19 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  data() {
+    return {
+      title: "Hello"
+    };
+  },
+  onLoad() {
+  },
+  methods: {}
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {
+    a: common_vendor.t($data.title)
+  };
+}
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "F:/HTML5/前端框架/记吧单词/pages/index/index.vue"]]);
+wx.createPage(MiniProgramPage);
