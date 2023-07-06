18203a5117e412306052745c90a8f1a8009fb532
哄哄 <1623511442@qq.com>
2023-07-06 15:11:51 +0800

    主页的修改

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -10,7 +10,17 @@
 			</view>
 		</view>
 		<!-- 页面内容 -->
-		<view>我是页面内容</view>
+		<view class="pagebody">
+			<view class="imgbox">
+				<image src="../../static/image/哆啦A梦.jpg" mode="widthFix" class="bodyImg"></image>
+			</view>
+			<view class="button-box" :style="{transform: `translate(0px,${screenHeight-statusBarHeight-440}px)`}">
+				<button class="mbutton uni-shadow-lg">开始作业</button>
+				<button class="mbutton uni-shadow-lg">听写</button>
+			</view>
+			<view class="bottomView" :style="{transform: `translate(0px,${screenHeight-statusBarHeight-44-300-50}px)`}">
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -22,6 +32,7 @@
 				statusBarHeight: 0,
 				// 导航栏高度
 				navBarHeight: 82 + 11,
+				screenHeight: 0
 			}
 		},
 		onLoad() {
@@ -33,19 +44,24 @@
 		//第一次加载时调用
 		created() {
 			//获取手机状态栏高度
-			this.statusBarHeight = uni.getSystemInfoSync()['statusBarHeight'];
+			var systemInfo = uni.getSystemInfoSync()
+			this.statusBarHeight = systemInfo['statusBarHeight'];
+			console.log(this.statusBarHeight);
+			this.screenHeight = systemInfo['screenHeight'];
 		},
 	}
 </script>
 
 <style>
-	.statusBar,.navBar{
+	.statusBar,
+	.navBar {
 		background-color: #2979ff;
 	}
-	
-	.navBar{
-		height: 44px;
+
+	.navBar {
+		height: 2.75rem;
 	}
+
 	.navBarBox .navBar {
 		padding: 3rpx 45rpx;
 		padding-bottom: 8rpx;
@@ -54,8 +70,50 @@
 		align-items: center;
 	}
 
-	.navTitle{
-		font-size: 16px;
+	.navTitle {
+		font-size: 1rem;
 		font-weight: 700;
+	}
+
+	.pagebody {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.imgbox {
+		background-color: #dcdcdc;
+		z-index: 0;
+	}
+
+	.bodyImg {
+		width: 100%;
+	}
+
+	.bottomView {
+		width: 100%;
+		border-radius: 1.5625rem 1.5625rem 0 0;
+		z-index: 1;
+		height: 18.75rem;
+		background-color: #94bcff;
+		position: absolute;
+	}
+	
+	.button-box{
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-around;
+		position: absolute;
+		z-index: 999;
+	}
+	.mbutton {
+		background-color: #2979ff;
+		color: white;
+		width: 100px;
+		height: 100px;
+		text-align: justify;
 	}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -6,7 +6,8 @@ const _sfc_main = {
       // 状态栏高度
       statusBarHeight: 0,
       // 导航栏高度
-      navBarHeight: 82 + 11
+      navBarHeight: 82 + 11,
+      screenHeight: 0
     };
   },
   onLoad() {
@@ -14,12 +15,17 @@ const _sfc_main = {
   methods: {},
   //第一次加载时调用
   created() {
-    this.statusBarHeight = common_vendor.index.getSystemInfoSync()["statusBarHeight"];
+    var systemInfo = common_vendor.index.getSystemInfoSync();
+    this.statusBarHeight = systemInfo["statusBarHeight"];
+    console.log(this.statusBarHeight);
+    this.screenHeight = systemInfo["screenHeight"];
   }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {
-    a: $data.statusBarHeight + "px"
+    a: $data.statusBarHeight + "px",
+    b: `translate(0px,${$data.screenHeight - $data.statusBarHeight - 440}px)`,
+    c: `translate(0px,${$data.screenHeight - $data.statusBarHeight - 44 - 300 - 50}px)`
   };
 }
 const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "F:/HTML5/前端框架/RememberWords/pages/index/index.vue"]]);
